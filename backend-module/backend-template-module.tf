7a2c501c5a466c36d5cd3c10cdbfec953e0c5200
elijah.kelim <elijah.kelim@whalemap.io>
2023-02-16 17:46:08 +0200

    Switch to modules + LoadBalancer;

---
+++ backend-module/backend-template-module.tf
@@ -0,0 +1,95 @@
+variable "name" {}
+
+variable "metadata" {}
+
+variable "network" {}
+
+
+resource "google_compute_http_health_check" "nginx" {
+  name               = "nginx-http-health-check"
+  request_path       = "/"
+  port               = "80"
+  check_interval_sec = 10
+  timeout_sec        = 1
+}
+
+resource "google_compute_instance_template" "nginx-instance-template" {
+  name         = "${var.name}-vm"
+  machine_type = "f1-micro"
+  tags         = ["ssh","http-server","https-server"]
+  can_ip_forward = true
+
+
+  disk {
+    source_image = "nginx-ubuntu-custom"
+    auto_delete = true
+    boot = true
+  }
+
+  network_interface {
+    subnetwork = var.network
+  }
+
+  metadata =  var.metadata
+
+  metadata_startup_script = <<-EOF
+  #!/bin/bash
+  WEB_PAGE_CONTENT=$(curl -H "Metadata-Flavor:Google" "http://metadata/computeMetadata/v1/instance/attributes/web_page_content")
+  export NGINX_MESSAGE="$WEB_PAGE_CONTENT"
+  envsubst < /etc/nginx/default_template.conf > /etc/nginx/default_template.conf.subst
+  sudo mv /etc/nginx/default_template.conf.subst /etc/nginx/conf.d/default.conf
+  service nginx restart
+EOF
+}
+
+resource "google_compute_target_pool" "nginx-pool" {
+  name = "${var.name}-pool"
+
+  health_checks = [
+    google_compute_http_health_check.nginx.self_link,
+  ]
+}
+
+resource "google_compute_instance_group_manager" "nginx-igm" {
+  name = "${var.name}-igm"
+
+  version {
+    instance_template = google_compute_instance_template.nginx-instance-template.id
+    name              = "primary"
+  }
+
+  target_pools       = ["google_compute_target_pool.nginx-pool.id"]
+  base_instance_name = "autoscaler-sample"
+}
+
+resource "google_compute_autoscaler" "nginx-autoscaler" {
+  name                  = "${var.name}-autoscaler"
+  target                = "google_compute_instance_group_manager.nginx-igm.id"
+  autoscaling_policy {
+    min_replicas = 1
+    max_replicas = 3
+    cooldown_period = 30
+    cpu_utilization {
+      target = 0.3 # Set low for testing purposes
+    }
+  }
+}
+
+resource "google_compute_backend_service" "backend_service" {
+  name = "${var.name}-http-backend"
+
+  backend {
+    group = google_compute_instance_group_manager.nginx-igm.self_link
+  }
+
+  health_checks = [
+    google_compute_http_health_check.nginx.self_link,
+  ]
+
+  protocol = "HTTP"
+}
+
+output "backend_service" {
+ value       = google_compute_backend_service.backend_service
+ description = "Backend service."
+}
--- main.tf
+++ main.tf
@@ -4,72 +4,70 @@ provider "google" {
   zone        = "us-central1-c"
 }
 
-resource "google_compute_instance" "example" {
-  name         = "example-instance"
-  machine_type = "f1-micro"
-  zone         = "us-central1-c"
+resource "google_compute_network" "vpc_network" {
+  name                    = "my-custom-mode-network"
+  auto_create_subnetworks = false
+  mtu                     = 1460
+}
 
+resource "google_compute_subnetwork" "default" {
+  name          = "my-custom-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.vpc_network.id
+}
+
+module "backend-1" {
+  source   = "./backend-module"
+  name     = "backend-1"
   metadata = {
     web_page_content = "Hello, Group 1!"
   }
-
-  boot_disk {
-    initialize_params {
-      image = "nginx-ubuntu-custom"
-    }
-  }
-
-  network_interface {
-    network = "default"
-    access_config {
-      // Ephemeral IP
-    }
-  }
-
-  metadata_startup_script = <<-EOF
-  #!/bin/bash
-  WEB_PAGE_CONTENT=$(curl -H "Metadata-Flavor:Google" "http://metadata/computeMetadata/v1/instance/attributes/web_page_content")
-  export NGINX_MESSAGE="$WEB_PAGE_CONTENT"
-  envsubst < /etc/nginx/default_template.conf > /etc/nginx/default_template.conf.subst
-  sudo mv /etc/nginx/default_template.conf.subst /etc/nginx/conf.d/default.conf
-  service nginx restart
-EOF
+  network = google_compute_subnetwork.default.id
 }
 
-#resource "google_compute_network" "vpc_network" {
-#  name                    = "my-custom-mode-network"
-#  auto_create_subnetworks = false
-#  mtu                     = 1460
-#}
-#
-#resource "google_compute_subnetwork" "default" {
-#  name          = "my-custom-subnet"
-#  ip_cidr_range = "10.0.1.0/24"
-#  region        = "us-central1-c"
-#  network       = google_compute_network.vpc_network.id
-#}
-#
-#resource "google_compute_instance_template" "custom-nginx" {
-#  name         = "nginx-vm"
-#  machine_type = "f1-micro"
-#  tags         = ["ssh","http-server","https-server"]
-#  can_ip_forward = true
-#
-#
-#  disk {
-#    source_image = "nginx-ubuntu-custom"
-#    auto_delete = true
-#    boot = true
-#  }
-#
-#  network_interface {
-#    subnetwork = google_compute_subnetwork.default.id
-#  }
-#  metadata = {
-#    web_page_content = "Hello from group 1"
-#  }
-#
-#  metadata_startup_script = "export NGINX_MESSAGE='{{ metadata[\"web_page_content\"] }}'\n"
-#
-#}
-#
+module "backend-2" {
+  source   = "./backend-module"
+  name     = "backend-2"
+  metadata = {
+    web_page_content = "Hello, Group 2!"
+  }
+  network = google_compute_subnetwork.default.id
+}
+
+
+resource "google_compute_forwarding_rule" "lb" {
+  name                  = "http-lb"
+  ip_address            = "ephemeral"
+  load_balancing_scheme = "EXTERNAL"
+  port_range            = "80"
+
+  target = google_compute_target_http_proxy.tp.self_link
+}
+
+resource "google_compute_target_http_proxy" "tp" {
+  name = "http-lb-proxy"
+
+  url_map = google_compute_url_map.url_map.self_link
+}
+
+resource "google_compute_url_map" "url_map" {
+  name = "http-lb-map"
+  default_service = module.backend-1.backend_service.id
+  host_rule {
+    hosts        = ["*"]
+    path_matcher = "path_matcher"
+  }
+  path_matcher {
+    name = "path_matcher"
+    default_service = module.backend-1.backend_service.id
+    path_rule {
+      paths = ["/backend-1/*"]
+      service = module.backend-1.backend_service.id
+    }
+    path_rule {
+      paths = ["/backend-2/*"]
+      service = module.backend-1.backend_service.id
+    }
+  }
+}
